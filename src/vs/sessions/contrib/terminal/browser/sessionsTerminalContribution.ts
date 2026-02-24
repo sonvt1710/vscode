13f353ad13f45da905c98d334d2857e65478ba7f
BeniBenj <besimmonds@microsoft.com>
2026-02-24 14:23:50 +0100

    :lipstick:

--- src/vs/sessions/contrib/terminal/browser/sessionsTerminalContribution.ts
+++ src/vs/sessions/contrib/terminal/browser/sessionsTerminalContribution.ts
@@ -19,6 +19,7 @@ import { ITerminalService } from '../../../../workbench/contrib/terminal/browser
 import { IPathService } from '../../../../workbench/services/path/common/pathService.js';
 import { Menus } from '../../../browser/menus.js';
 import { IActiveSessionItem, ISessionsManagementService } from '../../sessions/browser/sessionsManagementService.js';
+import { IsAuxiliaryWindowContext } from '../../../../workbench/common/contextkeys.js';
 
 /**
  * Returns the cwd URI for the given session: worktree for non-cloud agent
